96f4aef11e0c97366fc5e84fd7dae63b366a1146
yukkesan <yukke30dayo@gmail.com>
2022-05-06 20:18:07 +0900

    aaa

---
+++ FNModulesSample/Models/PrintErrorNodeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FNModulesSample.Models
+{
+	/// <summary>
+    /// エラー
+    /// </summary>
+	public class PrintErrorNodeModel
+	{
+		public PrintErrorNodeModel()
+		{
+		}
+	}
+}
+
